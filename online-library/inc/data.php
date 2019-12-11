f51409491992e5a8bbdb64154717d7a1bc0bcc26
mylennasr <49212873+mylennasr@users.noreply.github.com>
2019-12-11 18:52:50 -0500

    Add files via upload

---
+++ online-library/inc/data.php
@@ -0,0 +1,167 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+ "title" => "Hunger Makes the Wolf",
+ "img" => "img/media/HungerMakestheWolf.jpg",
+"genre" => "Paranormal Fantasy",
+"format" => "Paperback",
+"year" => 2017,
+"category" => "Books",
+"authors" => [
+    "Alex Wells"
+],
+"publisher" => "Angry Robot",
+"isbn" => '0857666444'
+];
+$catalog[102] = [
+ "title" => "Ordem Vermelha",
+ "img" => "img/media/OrdemVermelha.jpg",
+"genre" => "Fantasy",
+"format" => "Paperback",
+"year" => 2017,
+"category" => "Books",
+"authors" => [
+    "Felipe Castilho"
+],
+"publisher" => "Intrínseca",
+"isbn" => '8551002694'
+];
+$catalog[103] = [
+ "title" => "The Secret Place",
+ "img" => "img/media/TheSecretPlace.jpg",
+"genre" => "Mystery",
+"format" => "Paperback",
+"year" => 2014,
+"category" => "Books",
+"authors" => [
+    "Tana French"
+],
+"publisher" => "Penguin Books",
+"isbn" => '0143127519'
+];
+$catalog[104] = [
+ "title" => "Under the Dome",
+ "img" => "img/media/UndertheDome.jpg",
+"genre" => "Science Fiction",
+"format" => "Paperback",
+"year" => 2009,
+"category" => "Books",
+"authors" => [
+    "Stephen King"
+],
+"publisher" => "Gallery Books",
+"isbn" => '9780340992586'
+];
+//Movies
+$catalog[201] = [
+"title" => "Arrival",
+"img" => "img/media/Arrival.jpg",
+"genre" => "Science Fiction",
+"format" => "Streaming on Amazon Prime",
+"year" => 2016,
+"category" => "Movies",
+"director" => "Denis Villeneuve",
+"writers" => [
+    "Eric Heisserer"
+],
+"stars" => [
+    "Amy Adams",
+    "Forest Whitaker",
+    "Jeremy Renner",
+]
+];
+$catalog[202] = [
+"title" => "Into the Spiderverse",
+"img" => "img/media/IntotheSpiderverse.jpg",
+"genre" => "Animation, Adventure, Superhero",
+"format" => "Streaming on Netflix",
+"year" => 2018,
+"category" => "Movies",
+"director" => "Bob Persichetti",
+"writers" => [
+    "Phil Lord",
+    "Rodney Rothman"
+],
+"stars" => [
+    "Shameik Moore",
+    "Jake Johnson",
+    "Hailee Steinfeld",
+    "Mahershala Ali"
+]
+];
+$catalog[203] = [
+"title" => "Okja",
+"img" => "img/media/Okja.jpg",
+"genre" => "Science Fiction, Adventure, Drama",
+"format" => "Streaming on Netflix",
+"year" => 2017,
+"category" => "Movies",
+"director" => "Bong Joon-ho",
+"writers" => [
+    "Bong Joon-ho"
+],
+"stars" => [
+    "Ahn Seo-hyun",
+    "Paul Dano",
+    "Tilda Swinton"
+]
+];
+$catalog[204] = [
+"title" => "Star Wars: The Force Awakens",
+"img" => "img/media/TheForceAwakens.jpg",
+"genre" => "Space Opera",
+"format" => "Streaming on Disney+",
+"year" => 2015,
+"category" => "Movies",
+"director" => "J. J. Abrams",
+"writers" => [
+    "Lawrence Kasdan",
+    "J.J. Abrams",
+    "Michael Arndt"
+],
+"stars" => [
+    "Daisy Ridley",
+    "Carrie Fisher",
+    "Harrison Ford",
+    "John Boyega"
+  ]
+];
+//Music
+$catalog[301] = [
+"title" => "Dead Blue",
+"img" => "img/media/DeadBlue.jpg",
+"genre" => "Indie",
+"format" => "Available on Spotify",
+"year" => 2016,
+"category" => "Music",
+"artist" => "Still Corners"
+];
+$catalog[302] = [
+"title" => "Dirty Computer",
+"img" => "img/media/DirtyComputer.jpg",
+"genre" => "R&B, funk",
+"format" => "Available on Spotify",
+"year" => 2018,
+"category" => "Music",
+"artist" => "Janelle Monáe"
+];
+$catalog[303] = [
+"title" => "Perfect Velvet",
+"img" => "img/media/PerfectVelvet.jpg",
+"genre" => "Pop, R&B",
+"format" => "Available on Spotify",
+"year" => 2017,
+"category" => "Music",
+"artist" => "Red Velvet"
+];
+$catalog[304] = [
+"title" => "Visions",
+"img" => "img/media/Visions.jpg",
+"genre" => "Synthpop",
+"format" => "Available on Spotify",
+"year" => 2012,
+"category" => "Music",
+"artist" => "Grimes"
+];
+?>
